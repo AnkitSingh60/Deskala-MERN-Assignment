ebcc910314c0cc4be82f7c3964e7dd1fe20f004c
AnkitSingh60 <ankitanwar730@gmail.com>
2022-06-08 00:01:34 +0530

    added candidate page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,9 +4,37 @@ import Navbar from "./Navbar";
 const Home = () => {
   return (
     <>
-    <Navbar/>
-    <h1 className="App">Welcome</h1>
-    <h3 className="h3">Please Login/Signup to continue</h3>
+      <Navbar />
+      <h1 className="App">Welcome</h1>
+      <h3 className="h3">Please Login/Signup to continue</h3>
+
+      <div className="homeTable">
+        <table className="table">
+          <thead>
+            <tr className="table-dark">
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">DOB</th>
+              <th scope="col">Email</th>
+              <th scope="col">Result</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Ankit</td>
+              <td>17/06/1997</td>
+              <td>ankitanwar730@gmail.com</td>
+              <td className="d-flex justify-content-between">
+                <button>
+                  <i className="bi bi-pen"></i>
+                </button>
+                <button>delete </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
