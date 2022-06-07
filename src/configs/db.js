88e4c36f574008f70b9f82602d8b05a772e6bcce
AnkitSingh60 <ankitanwar730@gmail.com>
2022-06-07 15:33:15 +0530

    changes done

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 const connect = ()=>{
-    return mongoose.connect("mongodb+srv://ankit:ankit@cluster0.u0eil.mongodb.net/E_COMMERCE_MERN")
+    return mongoose.connect(process.env.MONGO_URI)
 }
 module.exports = connect;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 const express = require("express");
+const dotenv = require('dotenv');
+const cors = require("cors");
 const connect = require("./configs/db")
 // const userController = require("./controller/user.controller")
 const {login, register} = require("./controller/auth.controller")
 
+
 const app = express();
+//middlewares
+dotenv.config();
 app.use(express.json())
+
+const PORT = process.env.PORT || 5000;
+app.use(cors());
 
 app.post("/register",register)
 app.post("/login",login)
 // app.use("/users",userController)
 
-app.listen(2345, async () => {
+app.listen(PORT, async () => {
     try {
         await connect()
     } catch (error) {
         console.log('error:', error)
         
     }
-     console.log("Listening on port 2345");
+     console.log(`Listening on port ${PORT}...`);
  })
